da28eb5571034dcfcbaf5199cd8616b549c94c89
Badea-pro <79918603+Badea-pro@users.noreply.github.com>
2022-03-19 13:10:39 +0300

    Add files via upload

---
+++ frontend/src/Pages/index.js
@@ -0,0 +1,11 @@
+export { default as Login } from './Login'
+export { default as Register } from './Register'
+export { default as Welcome} from './Welcome'
+
+
+ (
+    <div className="Login">
+        <h1>For Login go to /login</h1>
+        <h1>For Register go to /register</h1>
+    </div>
+)
